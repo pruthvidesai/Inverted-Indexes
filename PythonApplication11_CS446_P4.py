93ee6d06a2f174329f95ff8593f8380989b052a9
Pruthvi Desai <pruthvidesai7@gmail.com>
2015-03-27 14:45:20 -0400

    Support multiple packets
    Successfully builds inverted indexes from given json data

--- PythonApplication11_CS446_P4.py
+++ PythonApplication11_CS446_P4.py
@@ -14,37 +14,46 @@ class Inverted():
         self.file = open(self.file)
         self.json_data = json.load(self.file)
         self.json_data = self.json_data['corpus']
-        #print self.json_data
 
     def query(self):
         pass
 
     def create_inverted_indexes(self):
-        # each packet in the list
+        # each packet in the corpus
         for packet in self.json_data:
-            # strip terms from text
             text = packet['text'].split()
-            length_text = len(text)
             # for term in each packet
-            for term in range(length_text):
-                # creates list of dicts for new term
+            for term in range(len(text)):
+                # creates a list of dictionaries for new term
                 if not self.inverted_index.has_key(text[term]):
-                    self.inverted_index[text[term]] = []
-                    term_dict = {}
-                    term_dict['playId'] = packet['playId']
-                    term_dict['sceneId'] = packet['sceneId']
-                    term_dict['sceneNum'] = packet['sceneNum']
-                    term_dict['pos'] = []
-                    self.inverted_index.get(text[term]).append(term_dict)
-                # for a term that already exists
+                    self.create_term(text[term], packet['playId'], packet['sceneId'], packet['sceneNum'])
+                
+                # if it's a different scene: create new dictionary
+                scene_exists = False
+                for dictionary in self.inverted_index.get(text[term]):
+                    if dictionary['sceneId'] == packet['sceneId']:
+                        scene_exists = True
+                if not scene_exists:
+                    self.create_term(text[term], packet['playId'], packet['sceneId'], packet['sceneNum'])
+                
+                # for a term that already exists: add position
                 for dicts in self.inverted_index.get(text[term]):
                     if dicts['sceneId'] == packet['sceneId']:
                         dicts['pos'].append(term + 1)
                 
+    def create_term(self, term, play, scene, num):
+        term_dict = {}
+        # creates list of dicts for new term
+        if not self.inverted_index.has_key(term):
+            self.inverted_index[term] = []
+        term_dict['playId'] = play
+        term_dict['sceneId'] = scene
+        term_dict['sceneNum'] = num
+        term_dict['pos'] = []
+        self.inverted_index.get(term).append(term_dict)
 
     def print_indexes(self):
         pprint(self.inverted_index)
-        #pprint(self.json_data)
 
 if __name__ == '__main__':
     file = "1.json"
